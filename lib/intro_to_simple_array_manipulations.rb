8917048295a5d061cd48bb5e928d208aaf39c024
Alex Merecka <merecka@gmail.com>
2019-02-02 00:34:37 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+def using_push(my_array, my_string)
+  my_array.push(my_string)
+end
+
+def using_unshift(some_array, some_string)
+  some_array.unshift(some_string)
+end
+
+def using_pop(my_array)
+  my_array.pop
+end
+
+def pop_with_args(my_array)
+  my_array.pop(2)
+end
+
+def using_shift(my_array)
+  my_array.shift
+end
+
+def shift_with_args(my_array)
+  my_array.shift(2)
+end
+
+def using_concat(my_array1, my_array2)
+  my_array1.concat(my_array2)
+end
+
+def using_insert(my_array, element1)
+  my_array.insert(4, element1)
+end
+
+def using_uniq(my_array)
+  my_array.uniq
+end
